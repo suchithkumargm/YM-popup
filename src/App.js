4d21c57196e0647cc17a1783fbab190c2993147a
Suchith Kumar G M <suchithkumargm@gmail.com>
2023-10-16 18:07:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import PopUp from './components/PopUp.js';
+import './App.css';
+
+const App = () => {
+
+	return (
+		<div className='main'>
+            <PopUp />
+		</div>
+	);
+}
+
+export default App;
